01add939ba484a23d28e8b045b066bfdc49e930d
sazzad <sazzad@ipvision-inc.com>
2015-04-11 17:59:10 +0600

    domain line added

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render_to_response
+from mysite.models import Line
+
 
 def home(request):
-	return render_to_response("mysite/home.html", {'hello': "hello world"})
+    line = Line(text="testing line one")
+    line.save()
+    return render_to_response("mysite/home.html", {'hello': "hello world"})
 
