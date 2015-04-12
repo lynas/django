67e7b169e46722790bc19ee5f70a916dc22883fc
sazzad <sazzad@ipvision-inc.com>
2015-04-12 18:26:38 +0600

    databe db table update done see commands

--- mysite/views.py
+++ mysite/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render_to_response
 from mysite.models import Line
+#from mysite.models import UsersOfIpv
 
 
 def home(request):
-    line = Line(text="testing line one")
-    line.save()
+    # line = Line(text="testing line one")
+    #line.save()
+    # user_of_ipv = UsersOfIpv(title="test", body="body")
+    # user_of_ipv.save()
+
     print Line.objects.all()
     return render_to_response("mysite/home.html", {'lines': Line.objects.all()})
 
